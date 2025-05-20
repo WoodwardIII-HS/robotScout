eeb500a01c61a17228abc5a3bc5e86e658d4f436
WoodwardIII-HS <gossf@region18.org>
2025-05-20 12:01:49 -0400

    Initial commit

---
+++ src/BlueAllianceEnhancedClient.java
@@ -0,0 +1,24 @@
+public class API {
+    ///set vars
+
+    //team numb
+
+    //year
+
+    //event
+
+    /// do stuff
+    //get robot name
+
+    //get team name
+
+    //last matches
+
+    //realtime opr with n matches
+
+    //opr piece 1 (n matches)
+
+    //opr piece 1 all time (all matches in season)
+
+
+}
